96eced83ca6b4c72cb2eabc91cc25a393ab7dc92
unknown <leonideko1@gmail.com>
2024-03-09 22:37:45 +0200

    add filtered for name or genres

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,4 +1,3 @@
-
 import configuration from "../configuration";
 
 const apiBasePath = `${configuration.apiUrl}/3`;
@@ -18,23 +17,24 @@ async function get<TBody>(relativeUrl: string): Promise<TBody> {
   return value;
 }
 
-export interface MovieDetails {
-  id: number;
-  title: string;
-  popularity: number;
-  overview: string;
-  backdrop_path?: string | null;
-}
-
 interface PageResponse<TResult> {
   page: number;
   results: TResult[];
   total_pages: number;
   total_results: number;
 }
-interface PageDetails<TResult> {
+
+interface MovieDetails {
+  id: number;
+  title: string;
+  overview: string;
+  popularity: number;
+  backdrop_path?: string | null;
+}
+
+interface PageDetails<T> {
+  results: T[];
   page: number;
-  results: TResult[];
   totalPages: number;
 }
 
@@ -44,18 +44,59 @@ interface Configuration {
   };
 }
 
+export interface KeywordItem {
+  id: number;
+  name: string;
+}
+
+export interface MoviesFilters {
+  keywords?: number[];
+  genres?: number[]
+}
+
 interface ITmbdClient {
   getConfiguration: () => Promise<Configuration>;
   getNowPlaying: (page: number) => Promise<PageDetails<MovieDetails>>;
+  getMovies: (page: number, filters: MoviesFilters) => Promise<PageDetails<MovieDetails>>;
+  getKeywords: (query: string) => Promise<KeywordItem[]>;
 }
 
 export const client: ITmbdClient = {
   getConfiguration: async () => {
     const response = await get<Configuration>("/configuration");
+
     return response;
   },
   getNowPlaying: async (page: number = 1) => {
     const response = await get<PageResponse<MovieDetails>>(`/movie/now_playing?page=${page}`);
+
+    return {
+      results: response.results,
+      totalPages: response.total_pages,
+      page: response.page,
+    };
+  },
+  getKeywords: async (query: string) => {
+    const response = await get<PageResponse<KeywordItem>>(`/search/keyword?query=${query}`);
+
+    return response.results;
+  },
+  getMovies: async (page: number, filters: MoviesFilters) => {
+    const params = new URLSearchParams({
+      page: page.toString(),
+    });
+
+    if (filters.keywords?.length) {
+      params.append("with_keywords", filters.keywords.join("|"));
+    }
+
+    if (filters.genres?.length) {
+      params.append("with_genres", filters.genres.join(","));
+    }
+
+    const query = params.toString();
+    const response = await get<PageResponse<MovieDetails>>(`/discover/movie?${query}`);
+
     return {
       results: response.results,
       totalPages: response.total_pages,
--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -1,14 +1,16 @@
-import { useContext, useEffect } from "react";
-import { fetchNextPage } from "./moviesSlice";
+import { useContext, useEffect, useState } from "react";
+import { fetchNextPage, resetMovies } from "./moviesSlice";
 import MovieCard from "./MovieCard";
 
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { Container } from "@mui/system";
-import { Grid, LinearProgress, Typography } from "@mui/material";
+import { Grid, LinearProgress } from "@mui/material";
 import { AuthContext, anonymousUser } from "../../AuthContext";
 import { useIntersectionObserver } from "../../hooks/useIntersectionObserver";
+import { Filters, MoviesFilter } from "./MoviesFilter";
 
 function Movies() {
+  const [filters, setFilters] = useState<Filters>();
   const dispatch = useAppDispatch();
   const movies = useAppSelector((state) => state.movies.top);
   const loading = useAppSelector((state) => state.movies.loading);
@@ -20,38 +22,56 @@ function Movies() {
   //підвантаження при прокрутці
   const [targetRef, entry] = useIntersectionObserver();
 
+useEffect(()=> {
+  dispatch(resetMovies())
+},[dispatch])
+
+
   useEffect(() => {
     if (entry?.isIntersecting && hasMorePages) {
-      dispatch(fetchNextPage());
+      const moviesFilters = filters
+      ? {
+        keywords : filters.keywords.map((k)=>k.id),
+        genres: filters?.genres,
+      }
+      : undefined;
+
+      dispatch(fetchNextPage(moviesFilters));
     }
-  }, [dispatch, entry?.isIntersecting, hasMorePages]);
+  }, [dispatch, entry?.isIntersecting, filters, hasMorePages]);
 
   return (
-    <Container sx={{ py: 8 }} maxWidth="lg">
-      <Typography variant="h4" align="center" gutterBottom>
-        Now playing
-      </Typography>
-
-      <Grid container spacing={4}>
-        {movies.map((item) => (
-          <Grid item key={item.id} xs={12} sm={6} md={4}>
-            <MovieCard
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              overview={item.overview}
-              popularity={item.popularity}
-              image={item.image}
-              enableUserActions={loggedIn}
-            />
-          </Grid>
-        ))}
+    <Grid container spacing={2} sx={{ flexWrap: "nowrap" }}>
+      <Grid item xs="auto">
+        <MoviesFilter
+          onApply={(f) => {
+            dispatch(resetMovies())
+            setFilters(f);
+          }}
+        />
       </Grid>
-      {/* коли йде скрол до низу буде відображатись лоадіндикатор */}
-      <div ref={targetRef}>
-       { loading && <LinearProgress color="secondary" sx={{ mt: 3 }} />}
-      </div>
-    </Container>
+      <Grid item xs={12}>
+        <Container sx={{ py: 8 }} maxWidth="lg">
+          <Grid container spacing={4}>
+            {movies.map((item) => (
+              <Grid item key={item.id} xs={12} sm={6} md={4}>
+                <MovieCard
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  overview={item.overview}
+                  popularity={item.popularity}
+                  image={item.image}
+                  enableUserActions={loggedIn}
+                />
+              </Grid>
+            ))}
+          </Grid>
+          {/* коли йде скрол до низу буде відображатись лоадіндикатор */}
+          <div ref={targetRef}>{loading && <LinearProgress color="secondary" sx={{ mt: 3 }} />}</div>
+        </Container>
+      </Grid>
+    </Grid>
   );
 }
 
